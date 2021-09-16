b0ebe0f652d08323c3762b43cf062da49f0f5501
Cameron Hardy <camhardy99@gmail.com>
2021-09-15 22:39:37 -0500

    Added 2 card limit - matchCheck and clickHandler have bugs

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,23 +2,57 @@ import React from "react";
 import Card from "../Card/Card";
 import "./Game.css";
 
-
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             cards: props.cards,
-        }
+        };
+        this.clicks = 0;
+        this.flipped = [];
     }
     clickHandler = (e) => {
-        e.target.firstChild.hidden = false;
+        this.clicks ++;
+        let timeoutID;
+        switch (true) {
+            case this.clicks === 1:
+                this.flipped.push(e.target.firstChild);
+                e.target.firstChild.hidden = false;
+                break;
+            case this.clicks === 2:
+                this.flipped.push(e.target.firstChild);
+                e.target.firstChild.hidden = false;
+                timeoutID = setTimeout(this.matchCheck, 2000);
+                break;
+            default:
+                clearTimeout(timeoutID);
+                this.flipped.forEach((i) => {
+                    i.hidden = true;
+                });
+                this.flipped = [];
+                this.clicks = 0;
+                break;
+        }
+    };
+    matchCheck = () => {
+        if(this.flipped[0].id === this.flipped[1].id){
+            this.flipped = [];
+            this.clicks = 0;
+            return;
+        } else {
+            this.flipped.forEach((i) => {
+                i.hidden = true;
+            });
+            this.flipped = [];
+            this.clicks = 0;
+        }
     }
     render() {
-        return(
+        return (
             <div onClick={this.clickHandler} className="cardContainer">
                 <Card amount={this.state.cards} />
             </div>
-        )
+        );
     }
 }
 
