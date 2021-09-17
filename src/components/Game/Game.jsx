f6bdb801dc712e15751896665461546303299f13
Cameron Hardy <camhardy99@gmail.com>
2021-09-17 09:05:40 -0500

    Refactored Game to move cards into state

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,44 +6,83 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: props.cards,
+            cardCount: props.cardCount,
+            gameStart: false,
+            cards: [],
+            clicks: 0,
         };
-        this.clicks = 0;
-        this.flipped = [];
     }
-    clickHandler = (e) => {
-        this.clicks ++;
-        switch (true) {
-            case this.clicks === 1:
-                this.flipped.push(e.target.firstChild);
-                e.target.firstChild.hidden = false;
-                break;
-            case this.clicks === 2:
-                this.flipped.push(e.target.firstChild);
-                e.target.firstChild.hidden = false;
-                setTimeout(this.matchCheck, 1000);
-                break;
-            default:
-                break;
-        }
+    componentDidMount = () => {
+        const cardArr = Card(this.state.cardCount);
+        this.setState({clicks: 10})
+        this.setState({cards: cardArr});
+        console.log("Clicks: ", this.state.clicks)
+        console.log("State Cards: ",this.state.cards)
+        //this.setState({ gameStart: true });
     };
-    matchCheck = () => {
-        if(this.flipped[0].id === this.flipped[1].id){
-            this.flipped = [];
-            this.clicks = 0;
-            return;
-        } else {
-            this.flipped.forEach((i) => {
-                i.hidden = true;
-            });
-            this.flipped = [];
-            this.clicks = 0;
-        }
-    }
+    // clickHandler = (e) => {
+    //     this.setState({ clicks: this.state.clicks++ });
+    //     switch (true) {
+    //         case this.state.clicks === 1:
+    //             this.setState({
+    //                 flipped: [
+    //                     ...this.state.flipped,
+    //                     {
+    //                         element: e.target.firstChild,
+    //                         key: e.target.firstChild.dataset.key,
+    //                     },
+    //                 ],
+    //             });
+    //             e.target.firstChild.hidden = false;
+    //             break;
+    //         case this.state.clicks === 2:
+    //             this.setState({
+    //                 flipped: [
+    //                     ...this.state.flipped,
+    //                     {
+    //                         element: e.target.firstChild,
+    //                         key: e.target.firstChild.dataset.key,
+    //                     },
+    //                 ],
+    //             });
+    //             e.target.firstChild.hidden = false;
+    //             setTimeout(this.matchCheck, 1000);
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // };
+    // matchCheck = () => {
+    //     if (
+    //         this.state.flipped[0].element.id ===
+    //         this.state.flipped[1].element.id
+    //     ) {
+    //         this.setState({
+    //             flipped: [],
+    //             clicks: 0,
+    //         });
+    //         return;
+    //     } else {
+    //         this.setState(() => {
+    //             flipped.forEach((i) => {
+    //                 i.element.hidden = true;
+    //             });
+    //         });
+    //         this.setState({
+    //             flipped: [],
+    //             clicks: 0,
+    //         });
+    //     }
+    // };
     render() {
         return (
-            <div onClick={this.clickHandler} className="cardContainer">
-                <Card amount={this.state.cards} />
+            <div>
+                {!this.state.gameStart && <h1>Loading...</h1>}
+                {this.state.gameStart && (
+                    <div onClick={this.clickHandler} className="cardContainer">
+                        {this.state.cards}
+                    </div>
+                )}
             </div>
         );
     }
